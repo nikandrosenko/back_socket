7427b717673284ab2d2d202efd73e1e27fd0bc7a
Nikita Androsenko <varzaid@gmail.com>
2023-06-26 00:31:30 +0300

    Создание приложения Quasar + socket.io

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { defineComponent } from "vue";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001");
+</script>
